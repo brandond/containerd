a26503e66e04c2d292ca6c871da516d83f650187
Vitor Savian <vitor.savian@suse.com>
2026-06-18 23:41:25 -0300

    Handle []byte envvar value
    Based on containerd/containerd#13453
    
    Signed-off-by: Vitor Savian <vitor.savian@suse.com>
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- internal/cri/store/container/metadata.go
+++ internal/cri/store/container/metadata.go
@@ -20,6 +20,9 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/containerd/containerd/v2/pkg/protobuf/proto"
+	oldruntime "github.com/containerd/containerd/v2/third_party/k8s.io/cri-api/pkg/apis/runtime/v1envstring"
+	"github.com/containerd/log"
 	runtime "k8s.io/cri-api/pkg/apis/runtime/v1"
 )
 
@@ -28,7 +31,8 @@ import (
 // 2) Metadata is checkpointed as containerd container label.
 
 // metadataVersion is current version of container metadata.
-const metadataVersion = "v1"
+const metadataVersion1 = "v1"
+const metadataVersion2 = "v2"
 
 // versionedMetadata is the internal versioned container metadata.
 type versionedMetadata struct {
@@ -39,7 +43,16 @@ type versionedMetadata struct {
 }
 
 // metadataInternal is for internal use.
-type metadataInternal Metadata
+type metadataInternal struct {
+	ID           string
+	Name         string
+	SandboxID    string
+	Config       json.RawMessage
+	ImageRef     string
+	LogPath      string
+	StopSignal   string
+	ProcessLabel string
+}
 
 // Metadata is the unversioned container metadata.
 type Metadata struct {
@@ -67,8 +80,8 @@ type Metadata struct {
 // MarshalJSON encodes Metadata into bytes in json format.
 func (c *Metadata) MarshalJSON() ([]byte, error) {
 	return json.Marshal(&versionedMetadata{
-		Version:  metadataVersion,
-		Metadata: metadataInternal(*c),
+		Version:  metadataVersion2,
+		Metadata: metadataToInternal(*c, metadataVersion2),
 	})
 }
 
@@ -80,9 +93,116 @@ func (c *Metadata) UnmarshalJSON(data []byte) error {
 	}
 	// Handle old version after upgrade.
 	switch versioned.Version {
-	case metadataVersion:
-		*c = Metadata(versioned.Metadata)
+	case metadataVersion1:
+		*c = internalv1ToMetadata(versioned.Metadata)
+		return nil
+	case metadataVersion2:
+		*c = internalv2ToMetadata(versioned.Metadata)
 		return nil
 	}
+
 	return fmt.Errorf("unsupported version: %q", versioned.Version)
+}
+
+func internalv1ToMetadata(c metadataInternal) Metadata {
+	config := &oldruntime.ContainerConfig{}
+	if err := json.Unmarshal(c.Config, config); err != nil {
+		log.L.WithError(err).WithField("container", c.ID).Error("Failed to unmarshal v1 container metadata config")
+	}
+	return Metadata{
+		ID:           c.ID,
+		Name:         c.Name,
+		SandboxID:    c.SandboxID,
+		Config:       upConvertContainerConfig(config),
+		ImageRef:     c.ImageRef,
+		LogPath:      c.LogPath,
+		StopSignal:   c.StopSignal,
+		ProcessLabel: c.ProcessLabel,
+	}
+}
+
+func internalv2ToMetadata(c metadataInternal) Metadata {
+	b := []byte{}
+	if err := json.Unmarshal(c.Config, &b); err != nil {
+		log.L.WithError(err).WithField("container", c.ID).Error("Failed to unmarshal v2 container metadata config as JSON")
+	}
+	config := &runtime.ContainerConfig{}
+	if err := proto.Unmarshal(b, config); err != nil {
+		log.L.WithError(err).WithField("container", c.ID).Error("Failed to unmarshal v2 container metadata config as protobuf")
+	}
+	return Metadata{
+		ID:           c.ID,
+		Name:         c.Name,
+		SandboxID:    c.SandboxID,
+		Config:       config,
+		ImageRef:     c.ImageRef,
+		LogPath:      c.LogPath,
+		StopSignal:   c.StopSignal,
+		ProcessLabel: c.ProcessLabel,
+	}
+}
+
+func metadataToInternal(c Metadata, version string) metadataInternal {
+	var config []byte
+	var err error
+	switch version {
+	case metadataVersion1:
+		// v1 stores ContainerConfig as JSON, with env var values as strings
+		config, err = json.Marshal(downConvertContainerConfig(c.Config))
+		if err != nil {
+			log.L.WithError(err).WithField("container", c.ID).Errorf("Failed to marshal %s container metadata config as JSON", version)
+		}
+	case metadataVersion2:
+		// v2 stores ContainerConfig marshalled as json []byte, holding marshalled protobuf
+		config, err = proto.Marshal(c.Config)
+		if err != nil {
+			log.L.WithError(err).WithField("container", c.ID).Errorf("Failed to marshal %s container metadata config as protobuf", version)
+		}
+		config, err = json.Marshal(config)
+		if err != nil {
+			log.L.WithError(err).WithField("container", c.ID).Errorf("Failed to marshal %s container metadata config as JSON", version)
+		}
+	default:
+		log.L.WithError(fmt.Errorf("unsupported version: %q", version)).WithField("container", c.ID).Error("Failed to marshal container metadata config")
+	}
+	return metadataInternal{
+		ID:           c.ID,
+		Name:         c.Name,
+		SandboxID:    c.SandboxID,
+		Config:       config,
+		ImageRef:     c.ImageRef,
+		LogPath:      c.LogPath,
+		StopSignal:   c.StopSignal,
+		ProcessLabel: c.ProcessLabel,
+	}
+}
+
+func upConvertContainerConfig(c *oldruntime.ContainerConfig) *runtime.ContainerConfig {
+	b, err := proto.Marshal(c)
+	if err != nil {
+		log.L.WithError(err).Error("failed to marshal ContainerConfig for upconvert")
+		return nil
+	}
+	o := &runtime.ContainerConfig{}
+	err = proto.Unmarshal(b, o)
+	if err != nil {
+		log.L.WithError(err).Error("failed to unmarshal ContainerConfig for upconvert")
+		return nil
+	}
+	return o
+}
+
+func downConvertContainerConfig(c *runtime.ContainerConfig) *oldruntime.ContainerConfig {
+	b, err := proto.Marshal(c)
+	if err != nil {
+		log.L.WithError(err).Error("failed to marshal ContainerConfig for downconvert")
+		return nil
+	}
+	o := &oldruntime.ContainerConfig{}
+	err = proto.Unmarshal(b, o)
+	if err != nil {
+		log.L.WithError(err).Error("failed to unmarshal ContainerConfig for downconvert")
+		return nil
+	}
+	return o
 }
